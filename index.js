14959dccc36ae696218b638001027a6e6c1e72f3
Miriama Svítková <miri52@users.noreply.github.com>
2021-06-10 12:30:46 +0100

    JavaScript Challenges 2 and 3, CSS update
    - fetch and display more data
    - beer styles, grid, flex

--- index.js
+++ index.js
@@ -8,15 +8,34 @@ const beersDiv = document.querySelector(".beers");
 //punk API limits every request to 25 beers
 async function getBeers() {
   try {
+    // fetch data
     const request = await fetch(urlBase);
     const beerData = await request.json();
 
+    // render data
     let beerHtml = "";
 
     for (let i = 0; i < beerData.length; i++) {
-      let beerName = beerData[i].name;
+      const beer = beerData[i];
       beerHtml += `
-      <h3>${beerName}</h3>`;
+      <div class='beer-wrapper card'>
+            <div class='beer'>
+                <img class='beer__img' src="${beer.image_url}">
+                <h3>${beer.name}</h3>
+                <span class='beer__info'>
+                    <span>ABV:${beer.abv}%</span>
+                    <span>IBU: ${beer.ibu} </span>
+                </span>
+            </div>
+            <div class='beer__content'>
+                <div class='beer__name'>${beer.name}</div>
+                <div class='beer__tagline'>${beer.tagline}</div>
+                <div class='beer__description'>${beer.description}</div>
+                <div class='beer__food-pairing'>
+                    Pair with: ${beer.food_pairing.join(", ")}
+                </div>
+            </div>
+        </div>`;
     }
 
     beersDiv.innerHTML = beerHtml;
